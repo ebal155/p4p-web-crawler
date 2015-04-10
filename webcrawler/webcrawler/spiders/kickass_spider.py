654fe96b6886809ae478c1f26cfd6539a16dc4d0
Earl Balmes <earl.balmes@gmail.com>
2015-04-10 14:57:48 +1200

    Added a function to parse through multiple pages

--- webcrawler/webcrawler/spiders/kickass_spider.py
+++ webcrawler/webcrawler/spiders/kickass_spider.py
@@ -8,19 +8,28 @@ class WarezbbSpider(scrapy.Spider):
     allowed_domains = ["https://kickass.to/"]
 
     start_urls = [
-       "https://kickass.to/movies"
+       "https://kickass.to"
     ]
 
+    #Make request to a number of pages of movies with parse_movie_page as a callback
     def parse(self, response):
-        
+        num_pages = 11
+
+        for x in range(0,11):
+            if (x == 0):
+                yield scrapy.Request(url="https://kickass.to/movies/",callback=self.parse_movie_page,dont_filter=True)
+            else:
+                yield scrapy.Request(url="https://kickass.to/movies/" + str(x) + "/",callback=self.parse_movie_page,dont_filter=True)
+
+    #Parse and make a request to each movie link inside a movie page with parse_movie as a callback
+    def parse_movie_page(self,response):
         list_movies_url = response.xpath(".//div[@class='markeredBlock torType filmType']/a/@href").extract()
 
         for url in list_movies_url:
             movie_page = "https://kickass.to" + url
             yield scrapy.Request(url=movie_page,callback=self.parse_movie,dont_filter=True)
-
-
-
+    
+    #Make a request to a movie page, and parse the relevant information
     def parse_movie(self,response):
         item = KickassItem()
 
