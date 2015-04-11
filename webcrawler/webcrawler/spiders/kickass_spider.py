a25cb46ff279f41a1259830799cf0dd504b6e4e6
mshehadeh <mshe246@gmail.com>
2015-04-11 15:55:38 +1200

    Code clean up in warezbb_spider.py & output is no longer in lists

--- webcrawler/webcrawler/spiders/kickass_spider.py
+++ webcrawler/webcrawler/spiders/kickass_spider.py
@@ -1,9 +1,8 @@
 
 import scrapy
 from webcrawler.items import KickassItem
-#KANyezus
 
-class WarezbbSpider(scrapy.Spider):
+class KickassSpider(scrapy.Spider):
     name = "kickass"
     allowed_domains = ["https://kickass.to/"]
 
