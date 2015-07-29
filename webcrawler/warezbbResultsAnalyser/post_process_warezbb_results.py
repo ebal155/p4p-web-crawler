9fba2891a7e270414e236a482d16f2310f029aa5
Mshehadeh <mshe246@gmail.com>
2015-07-30 11:14:34 +1200

    Cleaned up code_fields in warezbbResults

--- webcrawler/warezbbResultsAnalyser/post_process_warezbb_results.py
+++ webcrawler/warezbbResultsAnalyser/post_process_warezbb_results.py
@@ -5,6 +5,35 @@ class analyser():
     def __init__(self, filename):
         self.filename = filename
 
+    def post_process_code_fields(self):
+        with open(self.filename, 'rb') as preF:
+            postFile = open('postResultWarez.csv', 'wb')
+            wr = csv.writer(postFile, dialect='excel')
+            reader = csv.reader(preF)
+            rownum = 0
+            for row in reader:
+                if rownum == 0:
+                    header = row
+                    wr.writerow(header)
+                else:
+                    try:
+                        code_fields = row[0].split(",")
+                        for i in range(0,len(code_fields)):
+                            code = code_fields[i]
+                            code = code.strip()
+                            if "http://" in code or "https://" in code:
+                                code = code[code.index("//")+2:]
+                                if "www" in code:
+                                    code = code[4:]
+                                code = code[:code.index(".")]
+                                code_fields[i] = code
+                        row[0] = code_fields
+                        wr.writerow(row)
+                        print row[0]
+                    except Exception as e:
+                        print e
+                rownum += 1
+
     def total_author_count(self):
         with open(self.filename, 'rb') as f:
             reader = csv.reader(f)
@@ -62,4 +91,4 @@ class analyser():
 
 if __name__ == "__main__":
     my_analyser = analyser('warezbbMoviesResults.csv')
-    my_analyser.quality_count()
+    my_analyser.post_process_code_fields()
