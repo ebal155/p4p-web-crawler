569611206f7cad52d7348fb90b75823a7e96653a
mshehadeh <msh246@gmail.com>
2015-08-14 07:49:24 +1200

    add methods to calculate total/averages replies by authors

--- webcrawler/post processing/warezbb_data_analyser.py
+++ webcrawler/post processing/warezbb_data_analyser.py
@@ -9,14 +9,16 @@ class warezbb_data_analyser():
         self.print_total_author_count()
         self.print_author_view_averages()
         self.print_author_total_views()
+        self.print_author_total_replies()
+        self.print_author_replies_averages()
 
     def get_author_counts(self):
         """get all the author counts"""
         return self.myAnalyser.count_field('author', print_to_file=False,
             return_as_dict=True)
-        
+
     def get_author_total_views(self):
-        """Get all the views an author has ever seen"""
+        """Get all the views an author has ever had"""
         author_views = self.myAnalyser.count_two_fields('author', 'views')
         author_total_views = {}
         for author in author_views:
@@ -38,18 +40,48 @@ class warezbb_data_analyser():
                 pass
         return author_average_views
 
+    def get_author_total_replies(self):
+        """get all the replies an author has ever had"""
+        author_replies = self.myAnalyser.count_two_fields('author', 'replies')
+        author_total_replies = {}
+        for author in author_replies:
+            sum = 0
+            for view in author_replies[author]:
+                sum += int(view.replace(',',''))
+            author_total_replies[author] = sum
+        return author_total_replies
+
+    def caculate_author_replies_averages(self):
+        """Caculates the average amount of replies each author has"""
+        author_count = self.get_author_counts()
+        author_replies = self.get_author_total_replies()
+        author_average_replies = {}
+        for author in author_replies:
+            try:
+                author_average_replies[author] = author_replies[author] / author_count[author]
+            except:
+                pass
+        return author_average_replies
+
     def print_total_author_count(self):
-        self.myAnalyser.count_field('author', print_to_file=True)
+        self.myAnalyser.print_dict_to_csv(self.get_author_counts(),
+            'alltime_author_count.csv')
 
     def print_author_view_averages(self):
-        self.myAnalyser.print_to_file(self.caculate_author_view_averages, 
-            'overall_author_view_averages.csv')
+        self.myAnalyser.print_dict_to_csv(self.caculate_author_view_averages(), 
+            'alltime_author_view_averages.csv')
 
     def print_author_total_views(self):
-        self.myAnalyser.print_to_file(self.get_author_total_views,
-            'overall_author_total_views.csv')
+        self.myAnalyser.print_dict_to_csv(self.get_author_total_views(),
+            'alltime_author_views_total.csv')
 
-    
+    def print_author_total_replies(self):
+        self.myAnalyser.print_dict_to_csv(self.get_author_total_replies(),
+            'alltime_author_replies_total.csv')
+
+    def print_author_replies_averages(self):
+        self.myAnalyser.print_dict_to_csv(self.caculate_author_replies_averages(),
+            "alltime_author_replies_averages.csv")
 
 if "__main__" == __name__:
     jay = warezbb_data_analyser()
