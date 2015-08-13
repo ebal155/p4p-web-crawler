049cc85f8998678f06ee0ef2fa643563a7e8b796
mshehadeh <msh246@gmail.com>
2015-08-14 00:20:45 +1200

    Added new method in analyser that returns all rows sorted by a field

--- webcrawler/post processing/warezbb_data_analyser.py
+++ webcrawler/post processing/warezbb_data_analyser.py
@@ -5,6 +5,27 @@ class warezbb_data_analyser():
         self.filename = "processedWarezbbMovieFile.csv"
         self.myAnalyser = analyser(self.filename)
 
+    def print_all(self):
+        self.print_total_author_count()
+        self.print_author_view_averages()
+        self.print_author_total_views()
+
+    def get_author_counts(self):
+        """get all the author counts"""
+        return self.myAnalyser.count_field('author', print_to_file=False,
+            return_as_dict=True)
+        
+    def get_author_total_views(self):
+        """Get all the views an author has ever seen"""
+        author_views = self.myAnalyser.count_two_fields('author', 'views')
+        author_total_views = {}
+        for author in author_views:
+            sum = 0
+            for view in author_views[author]:
+                sum += int(view.replace(',',''))
+            author_total_views[author] = sum
+        return author_total_views
+
     def caculate_author_view_averages(self):
         """Caculates the average amount of views each author has"""
         author_count = self.get_author_counts()
@@ -17,32 +38,19 @@ class warezbb_data_analyser():
                 pass
         return author_average_views
 
-    def get_author_counts(self):
-        """get all the author counts"""
-        return self.myAnalyser.count_field('author', print_to_file=False, return_as_dict=True)
-
-    def get_author_total_views(self):
-        """Get all the views an author has ever seen"""
-        author_views = self.myAnalyser.count_two_fields('author', 'views')
-        author_total_views = {}
-        for author in author_views:
-            sum = 0
-            for view in author_views[author]:
-                sum += int(view.replace(',',''))
-            author_total_views[author] = sum
-        return author_total_views
-
     def print_total_author_count(self):
         self.myAnalyser.count_field('author', print_to_file=True)
 
     def print_author_view_averages(self):
-        self.myAnalyser.print_to_file(self.caculate_author_view_averages, 'overall_author_view_averages.csv')
+        self.myAnalyser.print_to_file(self.caculate_author_view_averages, 
+            'overall_author_view_averages.csv')
 
     def print_author_total_views(self):
-        self.myAnalyser.print_to_file(self.get_author_total_views, 'overall_author_total_views.csv')
+        self.myAnalyser.print_to_file(self.get_author_total_views,
+            'overall_author_total_views.csv')
 
     
 
 if "__main__" == __name__:
     jay = warezbb_data_analyser()
-    return jay.caculate_author_view_averages()
+    jay.print_all()
