889ba4f7717840a141a5eb9de89929e8a7869184
mshehadeh <msh246@gmail.com>
2015-08-14 11:11:22 +1200

    added methods to print out basic thread data

--- webcrawler/post processing/warezbb_data_analyser.py
+++ webcrawler/post processing/warezbb_data_analyser.py
@@ -4,13 +4,29 @@ class warezbb_data_analyser():
     def __init__(self):
         self.filename = "processedWarezbbMovieFile.csv"
         self.myAnalyser = analyser(self.filename)
+        self.years = ['2015', '2014', '2013', '2012', '2011', '2010', '2009']
 
     def print_all(self):
+        self.print_total_post_dates()
+        self.print_total_posts_by_year()
         self.print_total_author_count()
         self.print_author_view_averages()
         self.print_author_total_views()
         self.print_author_total_replies()
         self.print_author_replies_averages()
+        self.print_author_posts_all_years()
+        self.print_author_replies_all_years()
+
+    def get_post_dates(self):
+        """get all the posts dates"""
+        return self.myAnalyser.count_field('post_date', print_to_file=False,
+            return_as_dict=True)
+
+    def get_posts_dates_by_year(self):
+        """"gets all the posts sorted by year"""
+        return self.myAnalyser.count_field('year', print_to_file=False,
+            return_as_dict=True)
+
 
     def get_author_counts(self):
         """get all the author counts"""
@@ -63,6 +79,29 @@ class warezbb_data_analyser():
                 pass
         return author_average_replies
 
+    def get_author_posts_in_year(self, year):
+        author_count = self.myAnalyser.count_two_fields_matching_value('author',
+            'year', year)
+        return author_count
+
+    def get_author_total_replies_in_year(self, year):
+        author_replies = self.myAnalyser.count_two_fields_matching_third_field('author', 'replies', 'year', year) 
+        author_total_replies = {}
+        for author in author_replies:
+            sum = 0
+            for view in author_replies[author]:
+                sum += int(view.replace(',',''))
+            author_total_replies[author] = sum
+        return author_total_replies
+
+    def print_total_post_dates(self):
+        self.myAnalyser.print_dict_to_csv(self.get_post_dates(),
+            'alltime_posts.csv')
+
+    def print_total_posts_by_year(self):
+        self.myAnalyser.print_dict_to_csv(self.get_posts_dates_by_year(),
+            'alltime_posts_by_year.csv')
+
     def print_total_author_count(self):
         self.myAnalyser.print_dict_to_csv(self.get_author_counts(),
             'alltime_author_count.csv')
@@ -82,6 +121,25 @@ class warezbb_data_analyser():
     def print_author_replies_averages(self):
         self.myAnalyser.print_dict_to_csv(self.caculate_author_replies_averages(),
             "alltime_author_replies_averages.csv")
+
+    def print_author_posts_all_years(self):
+        for year in self.years:
+            self.print_author_posts_in_year(year)
+
+    def print_author_replies_all_years(self):
+        for year in self.years:
+            self.print_author_total_replies_in_year(year)
+
+    def print_author_posts_in_year(self, year):
+        self.myAnalyser.print_dict_to_csv(self.get_author_posts_in_year(year),
+            year + '_author_count.csv')
+
+    def print_author_total_replies_in_year(self, year):
+        self.myAnalyser.print_dict_to_csv(self.get_author_total_replies_in_year(year),
+            year + "_author_replies_total.csv")
+
+
+
 
 if "__main__" == __name__:
     jay = warezbb_data_analyser()
